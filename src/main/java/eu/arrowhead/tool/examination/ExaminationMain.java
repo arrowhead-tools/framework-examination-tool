2d3373950796261e58c3608016ae571c35060785
borditamas <borditamas@gmail.com>
2019-12-04 15:27:25 +0100

    checkCoreSystems impl

--- src/main/java/eu/arrowhead/tool/examination/ExaminationMain.java
+++ src/main/java/eu/arrowhead/tool/examination/ExaminationMain.java
@@ -10,12 +10,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 
 import eu.arrowhead.common.CommonConstants;
 import eu.arrowhead.common.Utilities;
+import eu.arrowhead.common.core.CoreSystem;
+import eu.arrowhead.common.dto.shared.SystemResponseDTO;
 import eu.arrowhead.tool.examination.config.ExaminationHttpService;
 import eu.arrowhead.tool.examination.config.HttpActor;
+import eu.arrowhead.tool.examination.controller.sysop_dto.SystemListResponseDTO;
 import eu.arrowhead.tool.examination.util.ExminationUtil;
+import eu.arrowhead.tool.examination.util.MgmtUri;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {CommonConstants.BASE_PACKAGE})
@@ -55,11 +60,28 @@ public class ExaminationMain implements ApplicationRunner {
 	private void checkCoreSystems() {
 		try {
 			httpService.sendRequest(HttpActor.SYSTEM_OPERATOR, Utilities.createURI(ExminationUtil.getUriScheme(sslEnabled), serviceReqistryAddress, serviceRegistryPort, CommonConstants.SERVICE_REGISTRY_URI + CommonConstants.ECHO_URI), HttpMethod.GET, String.class);
-			logger.info("Service Registry Core System is reachable on: " + serviceReqistryAddress + ":" + serviceRegistryPort);
-		} catch (Exception ex) {
-			logger.info("Service Registry Core System is not reachable on: " + serviceReqistryAddress + ":" + serviceRegistryPort);
+			logger.info(CoreSystem.SERVICE_REGISTRY.name() + " Core System is reachable on: " + serviceReqistryAddress + ":" + serviceRegistryPort);
+		} catch (final Exception ex) {
+			logger.info(CoreSystem.SERVICE_REGISTRY.name() + " Core System is not reachable on: " + serviceReqistryAddress + ":" + serviceRegistryPort);
 			logger.debug(ex.getMessage());
 		}
 		
+		final ResponseEntity<SystemListResponseDTO> systemListDTO = httpService.sendRequest(HttpActor.SYSTEM_OPERATOR, Utilities.createURI(ExminationUtil.getUriScheme(sslEnabled), serviceReqistryAddress, serviceRegistryPort, MgmtUri.GET_SYSTEMS), HttpMethod.GET, SystemListResponseDTO.class);
+		for (final SystemResponseDTO system : systemListDTO.getBody().getData()) {
+			for (final CoreSystem	coreSystem : CoreSystem.values()) {
+				if (system.getSystemName().equalsIgnoreCase(coreSystem.name()) && !system.getSystemName().equalsIgnoreCase(CoreSystem.SERVICE_REGISTRY.name())) {
+					final String address = system.getAddress();
+					final int port = system.getPort();
+					logger.info(coreSystem.name() + " is registered");
+					try {
+						httpService.sendRequest(HttpActor.SYSTEM_OPERATOR, Utilities.createURI(ExminationUtil.getUriScheme(sslEnabled), address, port, ExminationUtil.getCoreSystemUri(coreSystem) + CommonConstants.ECHO_URI), HttpMethod.GET, String.class);
+						logger.info(coreSystem.name() + " Core System is reachable on: " + address + ":" + port);
+					} catch (final Exception ex) {
+						logger.info(coreSystem.name() + " Core System is not reachable on: " + address + ":" + port);
+						logger.debug(ex.getMessage());
+					}
+				}
+			}
+		}
 	}
 }
--- src/main/java/eu/arrowhead/tool/examination/util/ExminationUtil.java
+++ src/main/java/eu/arrowhead/tool/examination/util/ExminationUtil.java
@@ -1,6 +1,10 @@
 package eu.arrowhead.tool.examination.util;
 
+import org.springframework.util.Assert;
+
 import eu.arrowhead.common.CommonConstants;
+import eu.arrowhead.common.core.CoreSystem;
+import eu.arrowhead.common.exception.InvalidParameterException;
 
 public class ExminationUtil {
 	
@@ -13,6 +17,29 @@ public class ExminationUtil {
 	//-------------------------------------------------------------------------------------------------
 	public static String getUriScheme(final boolean sslEnabled) {
 		return sslEnabled ? CommonConstants.HTTPS : CommonConstants.HTTP;
+	}
+	
+	//-------------------------------------------------------------------------------------------------
+	public static String getCoreSystemUri(final CoreSystem coreSystem) {
+		Assert.notNull(coreSystem, "coreSystem is null");
+		switch (coreSystem) {
+		case SERVICE_REGISTRY:
+			return CommonConstants.SERVICE_REGISTRY_URI;
+		case AUTHORIZATION:
+			return CommonConstants.AUTHORIZATION_URI;
+		case ORCHESTRATOR:
+			return CommonConstants.ORCHESTRATOR_URI;
+		case GATEKEEPER:
+			return CommonConstants.GATEKEEPER_URI;
+		case GATEWAY:
+			return CommonConstants.GATEWAY_URI;
+		case EVENT_HANDLER:
+			return CommonConstants.EVENT_HANDLER_URI;
+		case CHOREOGRAPHER:
+			return CommonConstants.CHOREOGRAPHER_URI;
+		default:
+			throw new InvalidParameterException(coreSystem + " not known");
+		}
 	}
 
 }

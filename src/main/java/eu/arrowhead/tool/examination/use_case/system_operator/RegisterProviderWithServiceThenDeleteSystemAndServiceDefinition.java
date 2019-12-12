aa221e54127d3df30d8ab645fc78112e53a0e36e
borditamas <borditamas@gmail.com>
2019-12-12 14:28:15 +0100

    RegisterProviderWithServiceThenDeleteSystemAndServiceDefinition use case update

--- src/main/java/eu/arrowhead/tool/examination/use_case/system_operator/RegisterProviderWithServiceThenDeleteSystemAndServiceDefinition.java
+++ src/main/java/eu/arrowhead/tool/examination/use_case/system_operator/RegisterProviderWithServiceThenDeleteSystemAndServiceDefinition.java
@@ -1,10 +1,14 @@
 package eu.arrowhead.tool.examination.use_case.system_operator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 import eu.arrowhead.common.dto.shared.ServiceDefinitionResponseDTO;
+import eu.arrowhead.common.dto.shared.ServiceInterfaceResponseDTO;
 import eu.arrowhead.common.dto.shared.ServiceRegistryRequestDTO;
 import eu.arrowhead.common.dto.shared.ServiceRegistryResponseDTO;
 import eu.arrowhead.common.dto.shared.SystemRequestDTO;
@@ -20,58 +24,89 @@ import eu.arrowhead.tool.examination.util.MgmtUri;
 public class RegisterProviderWithServiceThenDeleteSystemAndServiceDefinition extends SystemOperatorUseCase {
 	
 	//=================================================================================================
-	// members
-
-	//=================================================================================================
 	// methods
 	
 	//-------------------------------------------------------------------------------------------------
 	@Override
 	public void runUseCase() {
-		final SystemRequestDTO providerDTO = ExminationUtil.generateSystemRequestDTO();
-		final ServiceRegistryRequestDTO serviceRegistryRequestDTO = ExminationUtil.generateServiceRegistryRequestDTO(providerDTO);
-		final ResponseEntity<SystemResponseDTO> provider = request(HttpActor.SYSTEM_OPERATOR, CoreSystems.getServiceRegistryUri(MgmtUri.SERVICE_REGISTRY_SYSTEMS), HttpMethod.POST, SystemResponseDTO.class, providerDTO);
-		final ResponseEntity<ServiceRegistryResponseDTO> srEntry = request(HttpActor.SYSTEM_OPERATOR, CoreSystems.getServiceRegistryUri(MgmtUri.SERVICE_REGISTRY), HttpMethod.POST, ServiceRegistryResponseDTO.class, serviceRegistryRequestDTO);
+		final SystemRequestDTO providerRequest = ExminationUtil.generateSystemRequestDTO();
+		final ServiceRegistryRequestDTO serviceRegistryRequest = ExminationUtil.generateServiceRegistryRequestDTO(providerRequest);
 		
+		final ResponseEntity<SystemResponseDTO> providerResponse = request(HttpActor.SYSTEM_OPERATOR, CoreSystems.getServiceRegistryUri(MgmtUri.SERVICE_REGISTRY_SYSTEMS), HttpMethod.POST, SystemResponseDTO.class, providerRequest);
+		final ResponseEntity<ServiceRegistryResponseDTO> serviceRegistryResponse = request(HttpActor.SYSTEM_OPERATOR, CoreSystems.getServiceRegistryUri(MgmtUri.SERVICE_REGISTRY), HttpMethod.POST, ServiceRegistryResponseDTO.class, serviceRegistryRequest);
 		
-		assertNotNull(provider, "SystemResponse cannot be null");
-		assertNotNull(provider.getBody().getId(), "Provider id have to be defined in the reponse");
-		assertEqualsIgnoreCaseWithTrim(providerDTO.getSystemName(), provider.getBody().getSystemName(), "Provider name in DTO and in response must be the same");
-		assertEqualsIgnoreCaseWithTrim(providerDTO.getAddress(), provider.getBody().getAddress(), "Provider address in DTO and in response must be the same");
-		assertEqualsIgnoreCaseWithTrim(String.valueOf(providerDTO.getPort()), String.valueOf(provider.getBody().getPort()), "Provider port in DTO and in response must be the same");
-		if (providerDTO.getAuthenticationInfo() != null && !providerDTO.getAuthenticationInfo().isBlank()) {
-			assertEqualsIgnoreCaseWithTrim(providerDTO.getAuthenticationInfo(), provider.getBody().getAuthenticationInfo(), "Provider auth info in DTO and in response must be the same");
+		verifySystemResponse(providerRequest, providerResponse.getBody());
+		verifyServiceRegistryResponse(serviceRegistryRequest, serviceRegistryResponse.getBody());		
+		
+		request(HttpActor.SYSTEM_OPERATOR, CoreSystems.getServiceRegistryUri(MgmtUri.SERVICE_REGISTRY_SYSTEMS + "/" + String.valueOf(providerResponse.getBody().getId())), HttpMethod.DELETE, Void.class);
+		request(HttpActor.SYSTEM_OPERATOR, CoreSystems.getServiceRegistryUri(MgmtUri.SERVICE_REGISTRY_SERVICES + "/" + String.valueOf(serviceRegistryResponse.getBody().getServiceDefinition().getId())), HttpMethod.DELETE, Void.class);
+
+		verifyDeleteRequests(providerResponse.getBody(), serviceRegistryResponse.getBody());
+	}
+	
+	//=================================================================================================
+	// methods
+	
+	//-------------------------------------------------------------------------------------------------
+	private void verifySystemResponse(final SystemRequestDTO request, final SystemResponseDTO response) {
+		assertNotNull(response, "SystemResponse cannot be null");
+		assertNotNull(response.getId(), "Provider id have to be defined in the reponse");
+		assertEqualsIgnoreCaseWithTrim(request.getSystemName(), response.getSystemName(), "Provider name in request and in response must be the same");
+		assertEqualsIgnoreCaseWithTrim(request.getAddress(), response.getAddress(), "Provider address in request and in response must be the same");
+		assertEqualsIgnoreCaseWithTrim(String.valueOf(request.getPort()), String.valueOf(response.getPort()), "Provider port in request and in response must be the same");
+		if (request.getAuthenticationInfo() != null && !request.getAuthenticationInfo().isBlank()) {
+			assertEqualsIgnoreCaseWithTrim(request.getAuthenticationInfo(), response.getAuthenticationInfo(), "Provider auth info in request and in response must be the same");
+		}
+	}
+	
+	//-------------------------------------------------------------------------------------------------
+	private void verifyServiceRegistryResponse(final ServiceRegistryRequestDTO request, final ServiceRegistryResponseDTO response) {
+		assertNotNull(response, "ServiceRegistryResponse cannot be null");
+		assertNotNull(response.getId(), "SREntry id have to be defined in the reponse");
+		
+		assertEqualsIgnoreCaseWithTrim(request.getProviderSystem().getSystemName(), response.getProvider().getSystemName(), "Provider name in request and in response must be the same");
+		assertEqualsIgnoreCaseWithTrim(request.getProviderSystem().getAddress(), response.getProvider().getAddress(), "Provider address in request and in response must be the same");
+		assertEqualsIgnoreCaseWithTrim(String.valueOf(request.getProviderSystem().getPort()), String.valueOf(response.getProvider().getPort()), "Provider port in request and in response must be the same");
+		if (request.getProviderSystem().getAuthenticationInfo() != null && !request.getProviderSystem().getAuthenticationInfo().isBlank()) {
+			assertEqualsIgnoreCaseWithTrim(request.getProviderSystem().getAuthenticationInfo(), response.getProvider().getAuthenticationInfo(), "Provider auth info in request and in response must be the same");
 		}
 		
-		assertNotNull(srEntry, "ServiceRegistryResponse cannot be null");
-		assertNotNull(srEntry.getBody().getId(), "SREntry id have to be defined in the reponse");
-		assertEqualsIgnoreCaseWithTrim(serviceRegistryRequestDTO.getProviderSystem().getSystemName(), srEntry.getBody().getProvider().getSystemName(), "Provider name in serviceRegistryRequestDTO and in response must be the same");
-		assertEqualsIgnoreCaseWithTrim(serviceRegistryRequestDTO.getProviderSystem().getAddress(), srEntry.getBody().getProvider().getAddress(), "Provider address in serviceRegistryRequestDTO and in response must be the same");
-		assertEqualsIgnoreCaseWithTrim(String.valueOf(serviceRegistryRequestDTO.getProviderSystem().getPort()), String.valueOf(srEntry.getBody().getProvider().getPort()), "Provider port in serviceRegistryRequestDTO and in response must be the same");
-		if (serviceRegistryRequestDTO.getProviderSystem().getAuthenticationInfo() != null && !serviceRegistryRequestDTO.getProviderSystem().getAuthenticationInfo().isBlank()) {
-			assertEqualsIgnoreCaseWithTrim(serviceRegistryRequestDTO.getProviderSystem().getAuthenticationInfo(), srEntry.getBody().getProvider().getAuthenticationInfo(), "Provider auth info in serviceRegistryRequestDTO and in response must be the same");
+		assertEqualsIgnoreCaseWithTrim(request.getServiceDefinition(), response.getServiceDefinition().getServiceDefinition(), "ServiceDefinition in request and in response must be the same");
+		assertEqualsIgnoreCaseWithTrim(request.getServiceUri(), response.getServiceUri(), "ServiceURI in request and in response must be the same");
+		assertEqualsIgnoreCaseWithTrim(request.getSecure().name(), response.getSecure().name(), "Security level in request and in response must be the same");
+		assertEqualsIgnoreCaseWithTrim(String.valueOf(request.getVersion() == null ? 1 : request.getVersion()), String.valueOf(response.getVersion()), "Version of interface list in request and in response must be the same or 1 if it was not defined");
+		if (request.getEndOfValidity() != null && !request.getEndOfValidity().isBlank()) {
+			assertEqualsIgnoreCaseWithTrim(request.getEndOfValidity(), response.getEndOfValidity(), "EndOfValidity in request and in response must be the same");
 		}
-		assertEqualsIgnoreCaseWithTrim(serviceRegistryRequestDTO.getServiceDefinition(), srEntry.getBody().getServiceDefinition().getServiceDefinition(), "ServiceDefinition in serviceRegistryRequestDTO and in response must be the same");
-		assertEqualsIgnoreCaseWithTrim(serviceRegistryRequestDTO.getServiceUri(), srEntry.getBody().getServiceUri(), "ServiceURI in serviceRegistryRequestDTO and in response must be the same");
-		assertEqualsIgnoreCaseWithTrim(serviceRegistryRequestDTO.getSecure().name(), srEntry.getBody().getSecure().name(), "Security level in serviceRegistryRequestDTO and in response must be the same");
 		
-		request(HttpActor.SYSTEM_OPERATOR, CoreSystems.getServiceRegistryUri(MgmtUri.SERVICE_REGISTRY_SYSTEMS + "/" + String.valueOf(provider.getBody().getId())), HttpMethod.DELETE, Void.class);
-		request(HttpActor.SYSTEM_OPERATOR, CoreSystems.getServiceRegistryUri(MgmtUri.SERVICE_REGISTRY_SERVICES + "/" + String.valueOf(srEntry.getBody().getServiceDefinition().getId())), HttpMethod.DELETE, Void.class);
+		assertEqualsIgnoreCaseWithTrim(String.valueOf(request.getInterfaces().size()), String.valueOf(response.getInterfaces().size()), "Size of interface list in request and in response must be the same");		
+		List<String> responseInterfaceNames = new ArrayList<>();
+		for (ServiceInterfaceResponseDTO interface_ : response.getInterfaces()) {
+			responseInterfaceNames.add(interface_.getInterfaceName().toUpperCase().trim());
+		}
+		assertEqualsStringLists(request.getInterfaces(), responseInterfaceNames, "Interface list content in request and in response must be the same");
 		
+		if (request.getMetadata() != null) {			
+			assertEqualsStringStringMap(request.getMetadata(), response.getMetadata(), "Metadata content in request and in response must be the same");
+		}
+	}
+	
+	//-------------------------------------------------------------------------------------------------
+	private void verifyDeleteRequests(final SystemResponseDTO systemResponse, final ServiceRegistryResponseDTO serviceRegistryResponse) {
 		try {			
-			final ResponseEntity<SystemResponseDTO> systemResponse = request(HttpActor.SYSTEM_OPERATOR, CoreSystems.getServiceRegistryUri(MgmtUri.SERVICE_REGISTRY_SYSTEMS + "/" + String.valueOf(provider.getBody().getId())), HttpMethod.GET, SystemResponseDTO.class);
+			request(HttpActor.SYSTEM_OPERATOR, CoreSystems.getServiceRegistryUri(MgmtUri.SERVICE_REGISTRY_SYSTEMS + "/" + String.valueOf(systemResponse.getId())), HttpMethod.GET, SystemResponseDTO.class);
 		} catch (final Exception ex) {
 			assertExpectException(InvalidParameterException.class, ex, "No system id should exists after delete the provider");
 		}
 		
 		try {			
-			final ResponseEntity<ServiceRegistryResponseDTO> serviceRegistryResponse = request(HttpActor.SYSTEM_OPERATOR, CoreSystems.getServiceRegistryUri(MgmtUri.SERVICE_REGISTRY + "/" + String.valueOf(srEntry.getBody().getId())), HttpMethod.GET, ServiceRegistryResponseDTO.class);
+			request(HttpActor.SYSTEM_OPERATOR, CoreSystems.getServiceRegistryUri(MgmtUri.SERVICE_REGISTRY + "/" + String.valueOf(serviceRegistryResponse.getId())), HttpMethod.GET, ServiceRegistryResponseDTO.class);
 		} catch (final Exception ex) {
 			assertExpectException(InvalidParameterException.class, ex, "No srEntry id shloud exists after delete the provider system");
 		}
 		
 		try {			
-			final ResponseEntity<ServiceDefinitionResponseDTO> serviceDefResponse = request(HttpActor.SYSTEM_OPERATOR, CoreSystems.getServiceRegistryUri(MgmtUri.SERVICE_REGISTRY_SERVICES + "/" + String.valueOf(srEntry.getBody().getServiceDefinition().getId())), HttpMethod.GET, ServiceDefinitionResponseDTO.class);
+			request(HttpActor.SYSTEM_OPERATOR, CoreSystems.getServiceRegistryUri(MgmtUri.SERVICE_REGISTRY_SERVICES + "/" + String.valueOf(serviceRegistryResponse.getServiceDefinition().getId())), HttpMethod.GET, ServiceDefinitionResponseDTO.class);
 		} catch (final Exception ex) {
 			assertExpectException(InvalidParameterException.class, ex, "No service definition id should exists after delete the service definition");
 		}

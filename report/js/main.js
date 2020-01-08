456cd893d0e17924936b80436f78fff1c9fa2fc0
borditamas <borditamas@gmail.com>
2020-01-08 11:05:45 +0100

    add latency-csv div

--- report/js/main.js
+++ report/js/main.js
@@ -6,7 +6,19 @@ $(document).ready(function() {
 
 function readFullCSV() {
   $("#upload").bind("click", function () {
-    readFullCSVAssert();
+    $("#assert-sum-table").html('');
+    $("#assert-error-table").html('');
+    $("#assert-csv").html('');
+    $("#latency-csv").html('');
+
+    if ($("#fileUpload").val().toLowerCase().includes("assert")) {
+      readFullCSVAssert();
+    } else if ($("#fileUpload").val().toLowerCase().includes("latency")) {
+      readFullCSVLatency();
+    } else {
+      alert("Invalid file name!");
+      $("#fileUpload").val("");
+    }
   });
 }
 
@@ -36,6 +48,41 @@ function readFullCSVAssert() {
           reader.readAsText($("#fileUpload")[0].files[0]);
           reader.onloadend = function() {
             showUseCaseResults();
+          };
+      } else {
+          alert("This browser does not support HTML5.");
+      }
+  } else {
+      alert("Please upload a valid CSV file.");
+  }
+}
+
+function readFullCSVLatency() {
+  var regex = /^([a-zA-Z0-9\s_\\.\-:])+(.csv|.txt)$/;
+  if (regex.test($("#fileUpload").val().toLowerCase())) {
+      if (typeof (FileReader) != "undefined") {
+          var reader = new FileReader();
+          reader.onload = function (e) {
+              var table = $("<table id='full-latency-csv' />");
+              var rows = e.target.result.split("\n");
+              for (var i = 0; i < rows.length; i++) {
+                var row = $("<tr />");
+                var cells = rows[i].split(",");
+                if (cells.length > 1) {
+                    for (var j = 0; j < cells.length; j++) {
+                        var cell = $("<td />");
+                        cell.html(cells[j]);
+                        row.append(cell);
+                    }
+                    table.append(row);
+                }
+              }
+              $("#latency-csv").html('');
+              $("#latency-csv").append(table);
+          }
+          reader.readAsText($("#fileUpload")[0].files[0]);
+          reader.onloadend = function() {
+            showLatencyResults();
           };
       } else {
           alert("This browser does not support HTML5.");

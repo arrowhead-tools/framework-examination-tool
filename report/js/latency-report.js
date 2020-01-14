65a52b07d60c8a6dd3c6472d4c4810abe517b8f6
borditamas <borditamas@gmail.com>
2020-01-14 10:40:21 +0100

    async chart loading impl

--- report/js/latency-report.js
+++ report/js/latency-report.js
@@ -14,28 +14,32 @@ function showLatencyResults() {
     type: 'line'
   }]);
   getLatencyDataForChart();
-  $('#loading-div').hide();
 }
 
 function getLatencyDataForChart() {
-  var usecaseFilter = $('#usecase-filter').val();
-  var endpointFilter = $('#endpoint-filter').val();
+  return Promise.resolve().then(function() {
+    setTimeout(function() {
+      var usecaseFilter = $('#usecase-filter').val();
+      var endpointFilter = $('#endpoint-filter').val();
 
-  $.each($('#full-latency-csv tr'), function() {
-    var usecase = "";
-    var endpoint = "";
-    $.each(this.cells, function(cellIndex, cell) {
-      if (cellIndex == 0) {
-        usecase = cell.textContent;
-      }
-      if (cellIndex == 2) {
-        endpoint = cell.textContent;
-      }
+      $.each($('#full-latency-csv tr'), function() {
+        var usecase = "";
+        var endpoint = "";
+        $.each(this.cells, function(cellIndex, cell) {
+          if (cellIndex == 0) {
+            usecase = cell.textContent;
+          }
+          if (cellIndex == 2) {
+            endpoint = cell.textContent;
+          }
 
-      if (cellIndex == 3 && cell.textContent != 'latency_ms' && (usecase == usecaseFilter || usecaseFilter == "none") && (endpoint == endpointFilter || endpointFilter == "none")) {
-        var numb = cell.textContent.replace(/\D/g,'');
-        Plotly.extendTraces('latency-plotly-chart', { y:[[numb]] }, [0]);
-      }
-    });
+          if (cellIndex == 3 && cell.textContent != 'latency_ms' && (usecase == usecaseFilter || usecaseFilter == "none") && (endpoint == endpointFilter || endpointFilter == "none")) {
+            var numb = cell.textContent.replace(/\D/g,'');
+            Plotly.extendTraces('latency-plotly-chart', { y:[[numb]] }, [0]);
+          }
+        });
+      });
+      $('#loading-div').hide();
+    }, 0);
   });
 }
